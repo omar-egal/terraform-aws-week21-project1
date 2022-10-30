7af2c321a0419f2b488defdde39089efb8b3b880
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-10-30 05:57:41 +0000

    Bug fix

--- providers.tf
+++ providers.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  cloud {
-    organization = "omare-terraform"
-
-    workspaces {
-      name = "aws-two-tier-tf-cluod-cicd"
-    }
-  }
 }
 
 provider "aws" {
