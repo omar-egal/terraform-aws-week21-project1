c5c134e323dac8875f8ec32384d3b4916b819468
EC2 Default User <ec2-user@ip-172-31-68-230.ec2.internal>
2022-10-30 09:52:39 +0000

    Bug fix

--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 #---root/variables.tf
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+    type = string
+    default = "10.0.0.0/16"
+}
 variable "specific_ip" {}
 variable "key_name" {}
 variable "region" {}
